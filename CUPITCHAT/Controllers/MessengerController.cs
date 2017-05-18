f30d231a7ff9c58cee1aa6dc520a1d265f26f58f
Phan Dac Son <phandacson@live.com>
2017-05-18 22:45:32 +0700

    Welcome CUPIT CHAT

---
+++ CUPITCHAT/Controllers/MessengerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CUPITCHAT.Controllers
+{
+    public class MessengerController : Controller
+    {
+        // GET: Messenger
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CUPITCHAT/Controllers/MomentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CUPITCHAT.Controllers
+{
+    public class MomentController : Controller
+    {
+        // GET: Moment
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
